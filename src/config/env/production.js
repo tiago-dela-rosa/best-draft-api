f7e73202b958f8a8dd3f7f5dbebadf3c7695646a
Tiago Dela Rosa <t.dela.rosa.lopes@accenture.com>
2020-09-08 15:39:42 -0300

    adding prod env vars

--- src/config/env/production.js
+++ src/config/env/production.js
@@ -1,5 +1,25 @@
+import * as path from 'path';
+
 module.exports = {
-    api: {
-        port: process.env.PROD_PORT
-    }
-}
+  api: {
+    port: process.env.DEV_PORT,
+  },
+  db: {
+    client: 'pg',
+    connection: {
+      database: process.env.DEV_DB_DATABASE,
+      entities: [path.resolve(__dirname, '../../domain/models/**/index.js')],
+      host: process.env.DEV_DB_HOST,
+      password: process.env.DEV_DB_SECRET,
+      port: process.env.DEV_DB_PORT,
+      type: 'postgres',
+      username: process.env.DEV_DB_USER,
+    },
+    migrations: {
+      directory: './../../db/migrations',
+    },
+    seeds: {
+      directory: './../../db/seeds',
+    },
+  },
+};
