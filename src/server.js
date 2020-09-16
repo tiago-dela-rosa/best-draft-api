37fe123fcaca2b92641cb80438fa320da2340539
Tiago Dela Rosa <t.dela.rosa.lopes@accenture.com>
2020-09-15 21:31:49 -0300

    adding heroku port

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
-import app from "./app";
-import config from "./config";
-import logger from "./utils/logger";
+import app from './app';
+import config from './config';
+import logger from './utils/logger';
 
-app.listen(config.api.port || 8888, () => {
+app.listen(process.env.PORT || 8888, () => {
   logger.info(`aplication start listening on ${config.api.port}`);
 });
