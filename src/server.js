f907676586d0158ce2620ff4ca6540d471e90ce3
Tiago Dela Rosa <BJSSDEV\DEROTI>
2021-03-20 12:03:07 -0300

    Adding port following the log message

--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ import app from './app';
 import config from './config';
 import logger from './utils/logger';
 
-app.listen(process.env.PORT || 8888, '0.0.0.0', () => {
+app.listen(config.api.port || 8888, '0.0.0.0', () => {
   logger.info(`aplication start listening on ${config.api.port}`);
 });
