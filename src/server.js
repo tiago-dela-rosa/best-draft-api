80d9300705437d025ae351108a9d6d979a67c405
Tiago Dela Rosa <BJSSDEV\DEROTI>
2021-03-20 12:11:53 -0300

    testing port

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 import app from './app';
-import config from './config';
 import logger from './utils/logger';
 
-app.listen(config.api.port || 8888, '0.0.0.0', () => {
-  logger.info(`aplication start listening on ${config.api.port}`);
+const port = 8888;
+
+app.listen(port, '0.0.0.0', () => {
+  logger.info(`aplication start listening on ${port}`);
 });
